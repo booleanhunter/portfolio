f711df63497ce0b9e3dcee54febfbae3a256ffae
ashwin01 <ashwin01h@gmail.com>
2015-12-28 22:42:33 +0530

    Adding inline event handlers

---
+++ public/dist/js/custom-handlers.js
@@ -0,0 +1,32 @@
+function scrollToElement(elementClass){
+    $("."+elementClass).velocity("scroll", { 
+        duration: 1000,
+        easing: "easeInBack"
+    });
+}
+
+function toggleCollapse(event){
+    var box = event.currentTarget.parentElement.parentElement.parentElement,
+        boxBody = box.children[1],
+        icon = event.currentTarget.children[0];
+
+    if(box.className.indexOf('collapsed-box') !== -1) {
+        icon.className = icon.className.replace(/fa-plus/g, 'fa-minus');
+        $(boxBody).velocity('slideDown', {
+            duration: 500,
+            easing: 'easeInSine',
+            complete: function() {
+                box.className = box.className.replace(/ collapsed-box/g, '');
+            }
+        });
+    } else {
+        icon.className = icon.className.replace(/fa-minus/g, 'fa-plus');
+        $(boxBody).velocity('slideUp', {
+            duration: 500,
+            easing: 'easeInSine',
+            complete: function() {
+                box.className += ' collapsed-box';
+            }
+        });
+    }
+        }
